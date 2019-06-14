79a33a4cb4d3421efc53f763f039bff4bcf0f081
Wentao LI <liwentao90@yahoo.com>
2019-06-13 22:16:34 -0400

    - update next beta release version 1.0.0-beta.0 12#

--- src/OpenFinMock.ts
+++ src/OpenFinMock.ts
@@ -5,7 +5,9 @@ export class OpenFinMock{
     static generateMethods(name:string,Klass:any){
 
         const staticMethods = Klass.staticMethods||[];
+        const staticSyncMethods = Klass.staticSyncMethods||[];
         const instanceMethods = Klass.instanceMethods||[];
+        const instanceSyncMethods = Klass.instanceSyncMethods||[];
 
         for (const method of staticMethods){
             if (Klass[method]){
@@ -18,6 +20,32 @@ export class OpenFinMock{
 
                 if (args.length){
                     argsMsg = `This method also received the arguments: ${JSON.stringify(args,null,2)}`;
+                }
+
+                if (!OpenFinMock.silentMode){
+                    console.info(`[OpenFinMock] Static method \`${name}.${method}\` not implemented. 
+                        ${argsMsg} This method will not return anything, which will probably have unintended consequence.
+                        Implement this method on the \`${name}\` class to return a value.               
+                    `);
+                }
+            }
+        }
+
+        for (const method of staticSyncMethods){
+            if (Klass[method]){
+                console.warn(`[OpenFinMock] Warning! Attempt to overwrite already implemented static async methods:\`${name}.${method}\`. This method will not be overwritten. Remove this method from \`${name}.staticMethods\`.`);
+                continue;
+            }
+
+            Klass[method] = (...args:any[])=>{
+                let argsMsg = '';
+
+                if (args.length){
+                    argsMsg = `This method also received the arguments: ${JSON.stringify(args,null,2)}`;
+                }
+
+                if (args.length>1 && typeof args[args.length-2]=== 'function'){
+                    args[args.length-2]();
                 }
 
                 if (!OpenFinMock.silentMode){
@@ -42,6 +70,35 @@ export class OpenFinMock{
 
                 if (args.length){
                     argsMsg = `This method also received the arguments: ${JSON.stringify(args,null,2)}`;
+                }
+
+                if (!OpenFinMock.silentMode){
+                    console.info(`[OpenFinMock] Instance method \`${name}.${method}\` not implemented. 
+                        ${argsMsg} This method will not return anything, which will probably have unintended consequence.
+                        Implement this method on the \`${name}\` class to return a value.               
+                    `);
+                }
+
+            }
+        }
+
+        for (const method of instanceSyncMethods){
+            if (Klass.prototype[method]){
+                console.warn(`[OpenFinMock] Warning! Attemp to overwrite already implemented instance method: \`${name}#${method}\`.
+                    This method will not be overwritten. Remove this method from \`${name}.instanceMethods\`.       
+                `);
+                continue;
+            }
+
+            Klass.prototype[method] = async (...args:any[]) => {
+                let argsMsg = '';
+
+                if (args.length){
+                    argsMsg = `This method also received the arguments: ${JSON.stringify(args,null,2)}`;
+                }
+
+                if (args.length>1 && typeof args[args.length-2]=== 'function'){
+                    args[args.length-2]();
                 }
 
                 if (!OpenFinMock.silentMode){
--- src/desktop/Base.ts
+++ src/desktop/Base.ts
@@ -1,9 +1,9 @@
 export abstract class BaseApiClass {
 
-    static staticMethods:string[];
-    static staticSyncMethods:string[];
-    static instanceMethods:string[];
-    static instanceSyncMethods:string[];
+    static staticMethods?:string[];
+    static staticSyncMethods?:string[];
+    static instanceMethods?:string[];
+    static instanceSyncMethods?:string[];
 
     constructor(){
 
